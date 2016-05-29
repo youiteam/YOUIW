3b08d5f6a617aa96e86c7e1aa9d21b7b2eb96a6f
youiteam <2979316427@qq.com>
2016-05-29 12:10:34 +0000

    5

---
+++ MainView.java
@@ -0,0 +1,13 @@
+package com.bmob.lostfound;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainView extends Activity {
+	public void setContentView(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_view);
+	}
+
+}
